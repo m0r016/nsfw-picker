05142b9891c2ca7116d5879d67c3eb45ed49ada4
m0r016 <yuuki12_25@icloud.com>
2022-12-05 06:23:58 +0900

    feat: 削除オプションの追加

--- nsfw-picker.py
+++ nsfw-picker.py
@@ -3,6 +3,16 @@ import os
 import shutil
 import opennsfw2 as n2
 
+# コマンドライン引数を解析
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--delete", 
+    action="store_true", 
+    help="NSFW画像を削除します、注意してください")
+
+args = parser.parse_args()
+
 # 設定を読み込む
 config = ConfigParser()
 config.read("config.ini")
@@ -15,6 +25,9 @@ nsfw_dir = config["path"]["nsfw_dir"]
 
 # 閾値
 threshold = config["threshold"]["threshold"]
+
+# 拡張子が許可されているものか
+allow_extensions = ["jpg", "png", "jpeg"]
 
 # 画像を判定するディレクトリ内の全ての画像のパスを取得
 image_paths = []
@@ -30,9 +43,6 @@ for root, dirs, files in os.walk(image_dir):
         image_paths.append(image_path)
         print('image_path', image_path)
         print('image_paths', image_paths)
-
-# 拡張子が許可されているものか
-allow_extensions = ["jpg", "png", "jpeg"]
 
 # 全ての画像を順番に判定し、NSFW画像であれば別のディレクトリに移動
 for image_path in image_paths:
@@ -52,7 +62,9 @@ for image_path in image_paths:
     is_nsfw = n2.predict_image(image_path)
 
     if is_nsfw >= float(threshold):
-        # 画像がNSFWであれば、別のディレクトリに移動
-        shutil.copy(image_path, nsfw_dir)
-        print("NSFW: " + image_path)
-
+        if args.delete:
+            os.remove(image_path)
+        else:
+            # 画像がNSFWであれば、別のディレクトリに移動
+            shutil.copy(image_path, nsfw_dir)
+            print("NSFW: " + image_path)
