4ad40d8e90a59edd1af44ccbb1ef39cb3644db83
m0r016 <yuuki12_25@icloud.com>
2022-12-05 07:15:26 +0900

    feat: 複数のディレクトリから検出する

--- nsfw-picker.py
+++ nsfw-picker.py
@@ -8,38 +8,35 @@ from tqdm import tqdm
 config = ConfigParser()
 config.read("config.ini")
 
+# 画像を判定するディレクトリのパス
+image_dirs = config["path"]["image_dirs"]
+
 # NSFW画像を保存するためのディレクトリへのパス
 nsfw_dir = config["path"]["nsfw_dir"]
     
 # 閾値
 threshold = config["threshold"]["threshold"]
 
-# 複数のディレクトリから画像を検知する
-image_dirs = config["path"]["image_dirs"]
+# 画像を判定するディレクトリ内の全ての画像のパスを取得
+image_paths = []
+
+# 複数のディレクトリを指定して、指定された全てのディレクトリ内を再帰的に探索
 for image_dir in image_dirs:
+    for root, dirs, files in tqdm(
+        os.walk(image_dir),
+        desc="Reading images",
+        total=sum([len(files) for r, d, files in os.walk(image_dir)])):
 
-# 指定されたディレクトリ内を再帰的に探索
-for root, dirs, files in tqdm(
-    os.walk(image_dir),
-    desc="Reading images",
-    total=sum([len(files) for r, d, files in os.walk(image_dir)])):
+        # 探索されたディレクトリ内の全ての画像のパスを取得
+        for file_name in files:
 
-    # 指定されたディレクトリ内を再帰的に探索
-    for root, dirs, files in os.walk(image_dir):
-        # サブディレクトリは探索しない
-        dirs[:] = []
-
-        # 画像のパスを生成
-        image_path = os.path.join(root, file_name)
-        image_paths.append(image_path)
+            # 画像のパスを生成
+            image_path = os.path.join(root, file_name)
+            image_paths.append(image_path)
 
 # 拡張子が許可されているものか
 allow_extensions = ["jpg", "png", "jpeg"]
 
-# NSFW画像を保存するためのディレクトリが存在しない場合は作成する
-if not os.path.exists(nsfw_dir):
-    os.makedirs(nsfw_dir)
-    
 # 全ての画像を順番に判定し、NSFW画像であれば別のディレクトリに移動
 for image_path in tqdm(
     image_paths,
@@ -66,4 +63,3 @@ for image_path in tqdm(
         # 画像がNSFWであれば、別のディレクトリに移動
         shutil.copy(image_path, nsfw_dir)
         print("NSFW: " + image_path)
-
